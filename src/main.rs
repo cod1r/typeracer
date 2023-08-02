06c29fcfa499bf3cb1f7418dd2032f6a01b4bb20
cod1r <jasonho353@gmail.com>
2023-08-02 18:56:30 -0500

    applying the changes to the current terminal device

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use std::io::Read;
 use std::os::fd::AsRawFd;
 fn main() {
     let mut stdin = std::io::stdin();
     let mut termios = termios::Termios::from_fd(stdin.as_raw_fd()).expect("Termios struct");
     termios.c_cc[termios::VMIN] = 1;
+    termios.c_cc[termios::VTIME] = 0;
+    termios.c_lflag &= !termios::ICANON;
+    termios.c_lflag &= !termios::ECHO;
+    match termios::tcsetattr(stdin.as_raw_fd(), termios::TCSANOW, &mut termios) {
+        Ok(_) => {}
+        Err(_) => {
+            eprintln!("tcsetattr failed.");
+        }
+    }
+    let mut buf = [0; 1];
+    let _ = stdin.read(&mut buf);
+    termios.c_lflag |= termios::ICANON;
+    termios.c_lflag |= termios::ECHO;
+    match termios::tcsetattr(stdin.as_raw_fd(), termios::TCSANOW, &mut termios) {
+        Ok(_) => {}
+        Err(_) => {
+            eprintln!("tcsetattr failed.");
+        }
+    }
 }
