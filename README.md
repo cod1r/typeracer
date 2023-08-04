67cbb269546dc9043414340159ddfcc0cfab0dea
cod1r <jasonho353@gmail.com>
2023-08-04 09:16:12 -0500

    we know how to access the alternate screen buffer, clearing screen, using the ANSI escape codes. added the cheat sheet to the README

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # typeracer
 
 terminal typing application that allows you to practice typing.
+
+# References
+[ANSI Cheat Sheet](https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::Read;
+use std::io::Write;
 use std::os::fd::AsRawFd;
 fn main() {
     let mut stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
     let mut termios = termios::Termios::from_fd(stdin.as_raw_fd()).expect("Termios struct");
     termios.c_cc[termios::VMIN] = 1;
     termios.c_cc[termios::VTIME] = 0;
@@ -13,6 +15,24 @@ fn main() {
             eprintln!("tcsetattr failed.");
         }
     }
+    let _ = stdout.write(b"\x1b[?1049h\x1b[H\x1b[2J");
+    let _ = stdout.write(br#"OH MAY
+        sdfasdfasdfasdf
+        sdfasdfasdfasdfasdf
+        asdfa
+        sdf
+        asdfas
+        df
+        asdf
+        as
+        fd
+        asdf
+        as
+        fd
+        asdf
+        a"#);
+    let _ = stdout.write(b"\x1b[H");
+    let _ = stdout.flush();
     let mut buf = [0; 1];
     let _ = stdin.read(&mut buf);
     termios.c_lflag |= termios::ICANON;
@@ -23,4 +43,5 @@ fn main() {
             eprintln!("tcsetattr failed.");
         }
     }
+    let _ = stdout.write(b"\x1b[2J");
 }
