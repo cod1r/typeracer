63a7bc88b534f69edf1416a4844cbea4621d5e9a
cod1r <jasonho353@gmail.com>
2023-08-05 04:35:51 -0500

    Ignoring text files, while loop to continuously read from stdin, checking certain bytes in order to decide what action to take (moving cursor, deleting character, etc). Showing the right colors when typed character is wrong. ESC to leave the program

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::io::Read;
 use std::io::Write;
 use std::os::fd::AsRawFd;
 fn main() {
-    let _ = env::args().next();
+    let mut args_iter = env::args();
+    let _ = args_iter.next();
     let mut args = Vec::<String>::new();
-    while let Some(arg) = env::args().next() {
+    while let Some(arg) = args_iter.next() {
         args.push(arg);
     }
     let mut bytes = Vec::<u8>::new();
@@ -21,15 +22,15 @@ fn main() {
         typeracer <file>
 "#
                 );
+                return;
             }
-            _ => {
-                match std::fs::read(a) {
-                    Ok(bv) => bytes = bv,
-                    Err(_) => {
-                        eprintln!("reading {} failed", a);
-                    }
+            _ => match std::fs::read(a) {
+                Ok(bv) => bytes = bv,
+                Err(_) => {
+                    eprintln!("reading {} failed", a);
+                    return;
                 }
-            }
+            },
         },
         None => {
             println!(
@@ -41,6 +42,7 @@ fn main() {
         typeracer <file>
 "#
             );
+            return;
         }
     }
     let mut stdin = std::io::stdin();
@@ -54,19 +56,56 @@ fn main() {
         Ok(_) => {}
         Err(_) => {
             eprintln!("tcsetattr failed.");
+            return;
         }
     }
     let _ = stdout.write(b"\x1b[?1049h\x1b[H\x1b[2J");
+    let _ = stdout.write(&bytes);
     let _ = stdout.write(b"\x1b[H");
     let _ = stdout.flush();
     let mut buf = [0; 64];
-    let _ = stdin.read(&mut buf);
+    let mut sum_bytes = 0;
+    'outer: while sum_bytes < bytes.len() {
+        let n_read_from_stdin = match stdin.read(&mut buf) {
+            Ok(n) => n,
+            Err(_) => {
+                eprintln!("reading from stdin failed.");
+                return;
+            }
+        };
+        for idx in 0..n_read_from_stdin {
+            match buf[idx] {
+                8 => {
+                    if sum_bytes > 0 {
+                        sum_bytes -= 1;
+                        let _ = stdout.write(b"\x1b[1D");
+                        let _ = stdout.flush();
+                        continue;
+                    }
+                }
+                b'\n' => {}
+                b'\x1b' => {
+                    break 'outer;
+                }
+                _ => {}
+            }
+            if bytes[sum_bytes + idx] != buf[idx] {
+                let _ = stdout.write(b"\x1b[48;2;255;;m");
+            } else {
+                sum_bytes += 1;
+            }
+            let _ = stdout.write(&buf[idx..idx + 1]);
+            let _ = stdout.write(b"\x1b[0m");
+            let _ = stdout.flush();
+        }
+    }
     termios.c_lflag |= termios::ICANON;
     termios.c_lflag |= termios::ECHO;
     match termios::tcsetattr(stdin.as_raw_fd(), termios::TCSANOW, &mut termios) {
         Ok(_) => {}
         Err(_) => {
             eprintln!("tcsetattr failed.");
+            return;
         }
     }
     let _ = stdout.write(b"\x1b[2J");
