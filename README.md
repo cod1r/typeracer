62d6bd6ab2e6ae91d52bfc9b8f4c7718a368430e
cod1r <jasonho353@gmail.com>
2023-08-06 09:15:48 -0500

    Not doing terminal stuff anymore. Too tedious and a pain to handle. Switching to GUI iced.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # typeracer
 
-terminal typing application that allows you to practice typing.
-
-# References
-[ANSI Cheat Sheet](https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797)
+typing application that allows you to practice typing.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::io::Read;
 use std::io::Write;
-use std::os::fd::AsRawFd;
 fn main() {
     let mut args_iter = env::args();
     let _ = args_iter.next();
@@ -45,99 +44,4 @@ fn main() {
             return;
         }
     }
-    let mut stdin = std::io::stdin();
-    let mut stdout = std::io::stdout();
-    let mut termios = termios::Termios::from_fd(stdin.as_raw_fd()).expect("Termios struct");
-    termios.c_cc[termios::VMIN] = 1;
-    termios.c_cc[termios::VTIME] = 0;
-    termios.c_lflag &= !termios::ICANON;
-    termios.c_lflag &= !termios::ECHO;
-    match termios::tcsetattr(stdin.as_raw_fd(), termios::TCSANOW, &mut termios) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("tcsetattr failed.");
-            return;
-        }
-    }
-    let _ = stdout.write(b"\x1b[?1049h\x1b[H\x1b[2J");
-    let _ = stdout.write(&bytes);
-    let _ = stdout.write(b"\x1b[H");
-    let _ = stdout.flush();
-    let mut buf = [0; 64];
-    let mut cursor_pos = [0; 64];
-    let mut sum_bytes = 0;
-    'outer: while sum_bytes < bytes.len() {
-        let n_read_from_stdin = match stdin.read(&mut buf) {
-            Ok(n) => n,
-            Err(_) => {
-                eprintln!("reading from stdin failed.");
-                return;
-            }
-        };
-        let mut idx = 0;
-        while idx < n_read_from_stdin {
-            match buf[idx] {
-                b'\x7f' => {
-                    if sum_bytes > 0 {
-                        sum_bytes -= 1;
-                    }
-                    let _ = stdout.write(b"\x1b[6n");
-                    let _ = stdout.flush();
-                    let cursor_buf_len = match stdin.read(&mut cursor_pos) {
-                        Ok(n) => n,
-                        Err(_) => 0,
-                    };
-                    if matches!(
-                        cursor_pos[..cursor_buf_len][cursor_buf_len - 1 - 2..cursor_buf_len],
-                        [b';', b'1', b'R']
-                    ) {
-                        let _ = stdout.write(b"\x1b[1F");
-                        let _ = stdout.write(b"\x1b[999C");
-                    } else {
-                        let _ = stdout.write(b"\x1b[1D");
-                    }
-                    let _ = stdout.write(&bytes[sum_bytes..sum_bytes + 1]);
-                    if matches!(
-                        cursor_pos[..cursor_buf_len][cursor_buf_len - 1 - 2..cursor_buf_len],
-                        [b';', b'1', b'R']
-                    ) {
-                        let _ = stdout.write(b"\x1b[1F");
-                        let _ = stdout.write(b"\x1b[999C");
-                    } else {
-                        let _ = stdout.write(b"\x1b[1D");
-                    }
-                    let _ = stdout.flush();
-                    idx += 1;
-                    continue;
-                }
-                b'\n' | 11 | 12 => {
-                    idx += 1;
-                    continue;
-                }
-                b'\x1b' => {
-                    break 'outer;
-                }
-                _ => {}
-            }
-            if bytes[sum_bytes + idx] != buf[idx] {
-                let _ = stdout.write(b"\x1b[48;2;255;;m");
-            }
-            let _ = stdout.write(&buf[idx..idx + 1]);
-            let _ = stdout.write(b"\x1b[0m");
-            let _ = stdout.flush();
-            idx += 1;
-            sum_bytes += 1;
-        }
-    }
-    termios.c_lflag |= termios::ICANON;
-    termios.c_lflag |= termios::ECHO;
-    match termios::tcsetattr(stdin.as_raw_fd(), termios::TCSANOW, &mut termios) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("tcsetattr failed.");
-            return;
-        }
-    }
-    let _ = stdout.write(b"\x1b[2J");
-    let _ = stdout.write(b"\x1b[H");
 }
