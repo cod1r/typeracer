18ad6b5aa87c85eafb7b2067c3e9badcf84ff126
cod1r <jasonho353@gmail.com>
2023-08-07 01:41:36 -0500

    Changed the path text while input is changing, and reordered the elements in the column

--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,26 @@ impl Sandbox for TextDisplay {
     }
     fn update(&mut self, message: Self::Message) {
         match message {
-            Message::Path(s) => match std::fs::read(s) {
-                Ok(b) => match String::from_utf8(b) {
-                    Ok(s) => self.text = s,
+            Message::Path(s) => {
+                self.path_text = s.clone();
+                match std::fs::read(s) {
+                    Ok(b) => match String::from_utf8(b) {
+                        Ok(s) => self.text = s,
+                        Err(_) => {
+                            self.text = "file contents are not valid utf8".to_string();
+                        }
+                    },
                     Err(_) => {
-                        self.text = "file contents are not valid utf8".to_string();
+                        self.text = "file cannot be read".to_string();
                     }
-                },
-                Err(_) => {
-                    self.text = "file cannot be read".to_string();
                 }
-            },
+            }
         }
     }
     fn view(&self) -> Element<Message> {
         column![
+            text_input("path to file", self.path_text.as_str()).on_input(|s| Message::Path(s)),
             text(self.text.as_str()),
-            text_input("path to file", self.path_text.as_str()).on_input(|s| Message::Path(s))
         ]
         .into()
     }
